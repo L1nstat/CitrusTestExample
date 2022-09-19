d29956e36bb221fa92057bdb31782b4399e80c22
prsivak1 <prsivak1@mts.ru>
2022-09-19 17:12:23 +0300

    example with param

--- src/test/java/ru/L1nstat/example/tests/ExampleTest.java
+++ src/test/java/ru/L1nstat/example/tests/ExampleTest.java
@@ -1,8 +1,11 @@
 package ru.L1nstat.example.tests;
 
+import com.consol.citrus.annotations.CitrusResource;
 import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.context.TestContext;
 import com.consol.citrus.dsl.testng.TestNGCitrusTestRunner;
 import org.testng.Assert;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import ru.testit.annotations.DisplayName;
@@ -13,10 +16,11 @@ public class ExampleTest extends TestNGCitrusTestRunner {
     @DisplayName("example")
     @ExternalId("example")
     @Test
-    @Parameters(value = {"context"})
+    @Parameters(value = {"testContext"})
     @CitrusTest
-    public void example() {
-        Assert.assertEquals(true, true);
+    public void example(@Optional @CitrusResource TestContext testContext) {
+        testContext.setVariable("expectedValue", "string");
+        Assert.assertEquals("string", testContext.getVariable("expectedValue"));
     }
 
 }
